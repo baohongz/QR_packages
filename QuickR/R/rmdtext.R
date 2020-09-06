01ded63946992335b324d9deb9a8a0cfeabcb321
root <root@mail.quickr.ai>
2020-09-06 16:01:22 -0400

    More options

--- QuickR/R/rmdtext.R
+++ QuickR/R/rmdtext.R
@@ -2,9 +2,13 @@
 ##
 
 #' @export
-rmdtext <- function(text){
-  writeLines(paste0("---\noutput:\n  html_document:\n    code_folding: hide\n---\n\n",text), con="input.Rmd");
-  rmarkdown::render("input.Rmd", output_file="output.html");
+rmdtext <- function(text, out_options = NULL){
+  writeLines(paste0("---\noutput:\n  html_document:\n    code_folding: hide\n    self_contained: true\n---\n\n",text), con="input.Rmd");
+  if (is.null(out_options)) {
+    rmarkdown::render("input.Rmd", output_options = NULL, output_file="output.html");
+  } else {
+    rmarkdown::render("input.Rmd", output_options = jsonlite::fromJSON(out_options), output_file="output.html");
+  }
   invisible();
 }
 
